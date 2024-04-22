4bbd755e2905055176699a27268ee72831188cb6
sketchycrypt <sketchygfx@hotmail.com>
2024-04-22 23:29:01 +0100

    Refactor page components, update project page with new styling and icons, and remove unused files

--- src/app/projects/githubprojectsgrid.tsx
+++ src/app/projects/githubprojectsgrid.tsx
@@ -1,42 +1,56 @@
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-
-//cba to find a clever way to import
-import tailwindcssIcon from '../images/icons/tailwindcss.svg';
-import javascriptIcon from '../images/icons/javascript.svg';
-import typescriptIcon from '../images/icons/typescript.svg';
-import nextjsIcon from '../images/icons/nextdotjs.svg';
- 
-
+import React from "react";
+import Image from "next/image";
 
 interface GitHubProject {
- name: string;
- url: string;
- icon?: string;
+  name: string;
+  url: string;
+  icon?: string;
 }
 
 const projects: GitHubProject[] = [
- { name: 'new tab', url: 'https://github.com/sketchycrypt/new-tab', icon: tailwindcssIcon },
- { name: 'othello', url: 'https://github.com/sketchycrypt/Othello', icon: javascriptIcon },
- { name: 'crocrun', url: 'https://github.com/sketchycrypt/crocrun', icon: typescriptIcon },
- { name: 'portfolio', url: 'https://github.com/sketchycrypt/portfolio-website', icon: nextjsIcon },
+  {
+    name: "new tab",
+    url: "https://github.com/sketchycrypt/new-tab",
+    icon: "/icons/tailwindcss.svg",
+  },
+  {
+    name: "othello",
+    url: "https://github.com/sketchycrypt/Othello",
+    icon: "/icons/javascript.svg",
+  },
+  {
+    name: "crocrun",
+    url: "https://github.com/sketchycrypt/crocrun",
+    icon: "/icons/typescript.svg",
+  },
+  {
+    name: "portfolio",
+    url: "https://github.com/sketchycrypt/portfolio-website",
+    icon: "/icons/nextdotjs.svg",
+  },
 ];
 
 // TODO : find a way to implement the icons
 
 const GitHubProjectsGrid: React.FC = () => {
- return (
+  return (
     <div className="scale-125 md:scale-100 text-center grid grid-cols-2 gap-4 mx-auto my-16">
       {projects.map((project, index) => (
-        <div key={index} className="bg-black opacity-75 p-4 rounded-lg hover:opacity-100 -highlight transition ease-in-out duration-150">
-          <Link legacyBehavior href={project.url}>
-            <a className="text-center text-xl font-bold">{project.name}</a>
-          </Link>
+        <div
+          key={index}
+          className="bg-black opacity-75 p-4 rounded-lg hover:opacity-100 -highlight transition ease-in-out duration-150">
+          <div className="flex items-center justify-start">
+            <Image src={project.icon || ""} alt={project.name} width={48} height={48} />
+            <a
+              href={project.url}
+              className="text-center text-xl font-bold ml-4">
+              {project.name}
+            </a>
+          </div>
         </div>
       ))}
     </div>
- );
+  );
 };
 
 export default GitHubProjectsGrid;
--- src/app/svg.d.ts
+++
@@ -1,6 +0,0 @@
-export const icons = {
-    tailwindcss: require('../images/icons/tailwindcss.svg'),
-    javascript: require('../images/icons/javascript.svg'),
-    typescript: require('../images/icons/typescript.svg'),
-    nextjs: require('../images/icons/nextjs.svg'),
-};
