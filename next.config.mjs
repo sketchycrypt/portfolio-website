c9701956406cdba67daf3d676f7b0cba598e646c
sketchycrypt <sketchygfx@hotmail.com>
2024-04-23 13:02:33 +0100

    refactored page components plus changing icon directories

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '',
-    assetPrefix: '',
-    images: {
-      loader: 'default',
-      unoptimized: true,
-    },
-  }
+  basePath: "",
+  assetPrefix: "",
+  images: {
+    loader: "default",
+    unoptimized: true,
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+};
 
 export default nextConfig;
--- src/app/projects/githubprojectsgrid.tsx
+++ src/app/projects/githubprojectsgrid.tsx
@@ -1,54 +1,68 @@
 import React from "react";
 import Image from "next/image";
+import { StaticImageData } from "next/dist/shared/lib/get-img-props";
+
+import tailwindIcon from "../images/icons/tailwindcss.svg";
+import javascriptIcon from "../images/icons/javascript.svg";
+import typescriptIcon from "../images/icons/typescript.svg";
+import nextdotjsIcon from "../images/icons/nextdotjs.svg";
 
 interface GitHubProject {
   name: string;
   url: string;
-  icon?: string;
+  icon?: StaticImageData;
 }
 
 const projects: GitHubProject[] = [
   {
-    name: "new tab",
-    url: "https://github.com/sketchycrypt/new-tab",
-    icon: "/icons/tailwindcss.svg",
+    name: "Project1",
+    url: "url1",
+    icon: tailwindIcon,
   },
   {
-    name: "othello",
-    url: "https://github.com/sketchycrypt/Othello",
-    icon: "/icons/javascript.svg",
+    name: "Project2",
+    url: "url2",
+    icon: javascriptIcon,
   },
   {
-    name: "crocrun",
-    url: "https://github.com/sketchycrypt/crocrun",
-    icon: "/icons/typescript.svg",
+    name: "Project3",
+    url: "url3",
+    icon: typescriptIcon,
   },
   {
-    name: "portfolio",
-    url: "https://github.com/sketchycrypt/portfolio-website",
-    icon: "/icons/nextdotjs.svg",
+    name: "Project4",
+    url: "url4",
+    icon: nextdotjsIcon,
   },
 ];
-
-// TODO : find a way to implement the icons
 
 const GitHubProjectsGrid: React.FC = () => {
   return (
     <div className="scale-125 md:scale-100 text-center grid grid-cols-2 gap-4 mx-auto my-16">
-      {projects.map((project, index) => (
-        <div
-          key={index}
-          className="bg-black opacity-75 p-4 rounded-lg hover:opacity-100 -highlight transition ease-in-out duration-150">
-          <div className="flex items-center justify-start">
-            <Image src={project.icon || ""} alt={project.name} width={48} height={48} />
-            <a
-              href={project.url}
-              className="text-center text-xl font-bold ml-4">
-              {project.name}
-            </a>
+      {projects.map((project, index) => {
+        console.log(project.icon); // Logs the icon for each project
+        return (
+          <div
+            key={index}
+            className="bg-black opacity-75 p-4 rounded-lg hover:opacity-100 -highlight transition ease-in-out duration-150">
+            <div className="flex items-center justify-start">
+              {project.icon && (
+                <Image
+                  src={project.icon.src}
+                  alt={project.name}
+                  width={24}
+                  height={24}
+                />
+              )}
+              <a
+                href={project.url}
+                className="text-center text-xl font-bold ml-4">
+                {project.name}
+              </a>
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
