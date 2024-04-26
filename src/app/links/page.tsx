aa2cedfef99857eefbf6006cdcdba23056404230
sketchycrypt <sketchygfx@hotmail.com>
2024-04-26 18:46:15 +0100

    added openGraph image to metadata in layout.tsx and restyle navigation bar in page.tsx

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
           objectFit="cover"></Image>
       </div>
 
-      <div className="flex flex-col justify-center items-center h-screen place-items-center gap-4 md:gap-8 gap-y-12 md:gap-y-4 px-2 md:px-0">
+      <div className="font-inter flex flex-col justify-center items-center h-screen place-items-center gap-4 md:gap-8 gap-y-12 md:gap-y-4 px-2 md:px-0">
         <button
           className={`scale-125 md:scale-100 flex items-center justify-center bg-neutral-950 text-white px-6 py-4 text-lg rounded-full ${hoverEffect} z-20 w-48 `} onClick={() => (window.location.href = "https://twitter.com/sketchygfx__")}>
           <svg
@@ -57,7 +57,7 @@ export default function Home() {
           mastodon
         </button>
         <button
-          className={`scale-125 md:scale-100 flex items-center justify-center bg-neutral-950 text-white px-6 py-4 text-lg rounded-full ${hoverEffect} z-20 w-48`} onClick={() => (window.location.href = "https://mastodon.social/@skxtch")}>
+          className={`scale-125 md:scale-100 flex items-center justify-center bg-neutral-950 text-white px-6 py-4 text-lg rounded-full ${hoverEffect} z-20 w-48`} onClick={() => (window.location.href = "https://github.com/sketchycrypt")}>
           <svg
             className="fill-white mr-2 w-8 h-8"
             role="img"
@@ -84,19 +84,22 @@ export default function Home() {
         id="navigation-bar"
         className="hover:cursor-pointer z-30 md:text-2xl fixed top-0 w-full flex justify-center items-end text-xl font-inter font-bold text-gray-300 text-center">
         <Link href="../">
-          <div className="md:m-8 m-4 hover:scale-110 transition ease-in-out duration-150">
+        <div className="md:m-8 m-4 hover:scale-110 transition ease-in-out duration-150 flex flex-row items-center">
+          <svg className="fill-white mr-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21 13v10h-6v-6h-6v6h-6v-10h-3l12-12 12 12h-3zm-1-5.907v-5.093h-3v2.093l3 3z"/></svg>
             home
           </div>
         </Link>
          
         <Link legacyBehavior href="../projects">
-          <div className="md:m-8 m-4 hover:scale-110 transition ease-in-out duration-150">
+        <div className="md:m-8 m-4 hover:scale-110 transition ease-in-out duration-150 flex flex-row items-center">
+          <svg className="fill-white mr-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.602.075c2.201 1.174 4.904 3.254 6.398 5.252-1.286-.9-3.011-1.027-5.058-.549.222-1.469-.185-3.535-1.34-4.703zm-.825 6.925s1.522-7-3.335-7h-5.442v20h16v-10.629c0-3.42-4.214-3.116-7.223-2.371zm-10.777 15v-20h-2v22h18v-2h-16z"/></svg>
             projects
           </div>
         </Link>
 
         <Link legacyBehavior href="">
-          <div className="md:m-8 m-4 hover:scale-110 transition ease-in-out duration-150">
+        <div className="md:m-8 m-4 hover:scale-110 transition ease-in-out duration-150 flex flex-row items-center">
+          <svg className="fill-white mr-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6.188 8.719c.439-.439.926-.801 1.444-1.087 2.887-1.591 6.589-.745 8.445 2.069l-2.246 2.245c-.644-1.469-2.243-2.305-3.834-1.949-.599.134-1.168.433-1.633.898l-4.304 4.306c-1.307 1.307-1.307 3.433 0 4.74 1.307 1.307 3.433 1.307 4.74 0l1.327-1.327c1.207.479 2.501.67 3.779.575l-2.929 2.929c-2.511 2.511-6.582 2.511-9.093 0s-2.511-6.582 0-9.093l4.304-4.306zm6.836-6.836l-2.929 2.929c1.277-.096 2.572.096 3.779.574l1.326-1.326c1.307-1.307 3.433-1.307 4.74 0 1.307 1.307 1.307 3.433 0 4.74l-4.305 4.305c-1.311 1.311-3.44 1.3-4.74 0-.303-.303-.564-.68-.727-1.051l-2.246 2.245c.236.358.481.667.796.982.812.812 1.846 1.417 3.036 1.704 1.542.371 3.194.166 4.613-.617.518-.286 1.005-.648 1.444-1.087l4.304-4.305c2.512-2.511 2.512-6.582.001-9.093-2.511-2.51-6.581-2.51-9.092 0z"/></svg>
             links
           </div>
         </Link>
